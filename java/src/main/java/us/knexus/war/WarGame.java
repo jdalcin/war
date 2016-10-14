b6b86e4d7a92183623f8032f804e33642c078808
Justin <justin.karneeb@knexusresearch.com>
2016-10-14 18:42:31 -0400

    Base commit.

---
+++ java/src/main/java/us/knexus/war/WarGame.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package us.knexus.war;
+
+import java.util.ArrayList;
+import java.util.List;
+import us.knexus.deck.Card;
+import us.knexus.deck.CardFactory;
+import us.knexus.deck.Deck;
+
+/**
+ *
+ * @author Justin
+ */
+public class WarGame {    
+    public static void stepWar(Deck deck1, Deck deck2) {
+        
+    }
+    
+    public static void main(String[] args) {
+        // Get a new CardFactory instance
+        CardFactory factory = new CardFactory();
+        
+        // Get a full deck, set as player 1
+        Deck player1 = factory.createFullDeck();        
+        // Make a new empty deck for player 2
+        Deck player2 = new Deck();
+        // Shuffle the player 1 deck
+        player1.shuffle();
+
+        // deal half of player 1's shuffled deck to player 2
+        int deckSize = player1.getDrawPileSize() / 2;
+        for(int i=0; i<deckSize; ++i)
+            player2.addToDiscard(player1.draw());
+
+        // shuffle both decks
+        player1.shuffle();
+        player2.shuffle();
+
+        // step the game
+        stepWar(player1, player2);
+    }
+}
