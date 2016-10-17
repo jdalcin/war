6f913ae551bde0bb768fda20e92358b350823274
Justin <justin.karneeb@knexusresearch.com>
2016-10-17 15:41:43 -0400

    Modified WarGame to use 'playRound' instead of 'step' Added description pdf

--- java/src/main/java/us/knexus/war/WarGame.java
+++ java/src/main/java/us/knexus/war/WarGame.java
@@ -1,7 +1,32 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ *----------------------------------------------------------------
+ * @author: Justin
+ * Project Name: Programming Test
+ *----------------------------------------------------------------
+ * Description:(Class overview)
+ *  A programming test designed to use a Deck API to build a functioning
+ * version of the card game War.
+ *  
+ *----------------------------------------------------------------
+ * Copyright Notice: Knexus Research Corporation, ${date?date?string("yyyy")}
+ * All rights Reserved
+
+ *----------------------------------------------------------------
+ * Disclaimer:
+ * THIS SOFTWARE IS PROVIDED BY KNEXUS RESEARCH CORP., "AS IS" 
+ * AND ANY EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, WARRANTIES OF INFRINGEMENT AND THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL KNEXUS RESEARCH CORP.
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE DISTRIBUTION OR USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
  */
 package us.knexus.war;
 
@@ -9,11 +34,12 @@ import us.knexus.deck.CardFactory;
 import us.knexus.deck.Deck;
 
 /**
- *
+ * A simple versino of the card game WarGame
+ 
  * @author Justin
  */
 public class WarGame {    
-    public static void stepWar(Deck deck1, Deck deck2) {
+    public static void playRound(Deck deck1, Deck deck2) {
         
     }
     
@@ -38,6 +64,6 @@ public class WarGame {
         player2.shuffle();
 
         // step the game
-        stepWar(player1, player2);
+        playRound(player1, player2);
     }
 }
